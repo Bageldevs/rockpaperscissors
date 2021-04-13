5ab9e8eba832fdb3a15fc777f19f9e885942f85c
abid12508 <77800165+abid12508@users.noreply.github.com>
2021-04-13 10:05:31 -0500

    Update rock paper sicsors proj.py

--- rock paper sicsors proj.py
+++ rock paper sicsors proj.py
@@ -1,14 +1,15 @@
 import random
-print("rock, paper scissors")
 
-def rockpaperscissors():
+
+def main():
+    print("rock, paper scissors")
     if input() == "rock":
         if random.randrange(1, 3) == 1:
             print("rock, draw!")
         elif random.randrange(1, 3) == 2:
             print("paper, I win!")
         else:
-            print("scissors ,Nooo I lost!")
+            print("scissors, Nooo I lost!")
     #if you want to type paper, type paper 2 times since it is an elif condition
     elif input() == "paper":
         if random.randrange(1, 3) == 1:
@@ -25,7 +26,7 @@ def rockpaperscissors():
             print("paper, Noo I lost!")
         else:
             print("Scissors, Draw!")
-rockpaperscissors()
+main()
 
 
 
