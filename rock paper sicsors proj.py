c3a13aad38bc673ae14690effeaff5b0734a2786
abid12508 <77800165+abid12508@users.noreply.github.com>
2021-04-14 08:57:04 -0500

    Rock Paper Scissors with Tkinter

--- rock paper sicsors proj.py
+++ rock paper sicsors proj.py
@@ -1,32 +1,50 @@
+from tkinter import *
 import random
-print("rock, paper, or scissors")
-def calculation():
-    random.randrange(1, 3)
-def rockpaperscissors():
-    if input() == "rock":
-        if calculation == 1:
-            print("rock, draw!")
-        elif calculation == 2:
-            print("paper, I win!")
-        else:
-            print("scissors ,Nooo I lost!")
-    #if you want to type paper, type paper 2 times since it is an elif condition
-    elif input() == "paper":
-        if calculation == 1:
-            print("rock, Nooo I lost!")
-        elif calculation == 2:
-            print("Paper, Draw!")
-        else:
-            print("Scissors, Haha I got you!")
-    #same goes here, type it 3 times if you want it to be scissors
-    elif input() == "scissors":
-        if calculation == 1:
-            print("rock, Haha I got you!")
-        elif calculation == 2:
-            print("paper, Noo I lost!")
-        else:
-            print("Scissors, Draw!")
-rockpaperscissors()
+root = Tk()
+root.title('Rock Paper Scissors!')
+root.configure(bg='grey')
+frame = Frame(root)
+frame.pack()
+rightFrame = Frame(root)
+rightFrame.pack(side=RIGHT)
+bottomFrame = Frame(root)
+bottomFrame.pack(side=LEFT)
+leftFrame = Frame(root)
+leftFrame.pack(side=LEFT)
+topFrame = Frame(root)
+topFrame.pack(side=TOP)
+text = Text(root)
+text.insert(INSERT, "Rock Paper Scissors!")
+text.pack(side=TOP)
+text.configure(bg='lightblue')
+def calculationforrock():
+    if random.randrange(1, 3) == 1:
+        text.insert(INSERT, "     rock, Draw!     ")
+    elif random.randrange(1, 3) == 2:
+        text.insert(INSERT, "    Paper, Haha I got you!     ")
+    else:
+        text.insert(INSERT, "     scissors, Nooo you got me!     ") 
+def calculationforpaper():
+    if random.randrange(1, 3) == 1:
+        text.insert(INSERT,"     Rock, Nooo I lost!     ")
+    elif random.randrange(1, 3) == 2:
+        text.insert(INSERT, "     Paper, Draw!     ")
+    else:
+        text.insert(INSERT, "     Scissors, Haha I win!     ")
+def calculationforscissors():
+    if random.randrange(1, 3) == 1:
+        text.insert(INSERT, "     rock, Haha I win!     ")
+    elif random.randrange(1, 3) == 2:
+        text.insert(INSERT, "     Paper, Nooo I lost!     ")
+    else:
+        text.insert(INSERT, "     Scissors, Draw!     ")
 
+B = Button(root, text ="rock", fg="red", command=calculationforrock)
+C = Button(root, text="paper", fg="green", command=calculationforpaper)
+D = Button(root, text="scissors", fg="blue", command=calculationforscissors)
 
+B.pack()
+C.pack()
+D.pack()
 
+root.mainloop()
